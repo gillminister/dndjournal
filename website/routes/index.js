1534cea321d590926cb43dd5ceb7c09802aefb40
dendril <jorn91@gmail.com>
2017-01-31 23:42:52 +0100

    styling and separated character from index

--- website/routes/index.js
+++ website/routes/index.js
@@ -6,5 +6,10 @@ router.get('/', function (req, res, next) {
     res.render('index');
 });
 
+/* GET character page. */
+router.get('/character/', function (req, res, next) {
+    res.render('character');
+});
+
 
 module.exports = router;
