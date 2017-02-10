c5e288776576440635308b440b4367bb9b9fb91c
dendril <jorn91@gmail.com>
2017-02-11 00:42:55 +0100

    added entity character and populate database script

--- psql-queries/update_all_queries.sh
+++ psql-queries/update_all_queries.sh
@@ -1,4 +1,7 @@
 psql westplate -f enum/skill_name.psql
 
+psql westplate -f entity/character.psql
 psql westplate -f entity/character_class.psql
 psql westplate -f entity/class_skill_list.psql
+
+psql westplate -f populate/populate-testing.psql
